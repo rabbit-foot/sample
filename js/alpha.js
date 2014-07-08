8ed6f5cc55a27abe6f42eb177ee49b698cca39a7
rabbit-foot <paperkraft@gmail.com>
2014-07-08 10:57:02 +0900

    Javascript files
    jsファイル一式

---
+++ js/alpha.js
@@ -0,0 +1,23 @@
+$(function(){
+  $(".alpha").hover(function(){
+    $(this).fadeTo("normal", 0.2);
+  },function(){
+    $(this).fadeTo("normal", 1.0);
+  });
+});
+
+$(function(){
+  $(".alpha2").hover(function(){
+    $(this).fadeTo("normal", 0.5);
+  },function(){
+    $(this).fadeTo("normal", 1.0);
+  });
+});
+
+$(function(){
+  $(".alpha3").hover(function(){
+    $(this).fadeTo("normal", 0.1);
+  },function(){
+    $(this).fadeTo("normal", 1.0);
+  });
+});
